8e52f84d3a2936900102206b1bdfe587f1f5053f
ricguer <rj.guerra@outlook.com>
2023-07-09 22:35:10 -0400

    Create First Route to Get All Employees From Database

--- server.js
+++ server.js
@@ -10,16 +10,32 @@ const app = express();                                          /* Initialize ex
                                                                 /* Define middleware to use for server                  */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use((req, res) => res.status(404).end());
 
                                                                 /* Create connection to employee tracker database       */
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
         password: "mysql",
-        database: "employeeTracker_db"
-    }
-)
+        database: "employeetracker_db"
+    },
+    console.log("Connected to the employeetracker_db database")
+);
+
+                                                                /* ===================== ROUTES ======================= */
+                                                                
+                                                                /* Route to view all employees                          */
+app.get("/api/employees", (req, res) => {
+    const sql = `SELECT * FROM employee`;
+    connection.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({ employees: rows });
+    });
+});
 
                                                                 /* Have server listen to dedicated port                 */
 app.listen(PORT, () => {
