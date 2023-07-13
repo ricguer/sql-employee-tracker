089352e2cb6b16a3b5b2e8dadfb5a7bd01097cbe
ricguer <rj.guerra@outlook.com>
2023-07-13 19:02:55 -0400

    Auto Increment ID of Each Table

--- db/schema.sql
+++ db/schema.sql
@@ -8,25 +8,25 @@ USE employeetracker_db;                                         /* Use employee 
 
                                                                 /* ---------------- Table: Department ----------------- */
 CREATE TABLE department (
-    id              INT          NOT NULL  PRIMARY KEY,
+    id              INT          NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
     name            VARCHAR(30)  NOT NULL
 );
 
                                                                 /* ------------------- Table: Role -------------------- */
 CREATE TABLE role (
-    id             INT           NOT NULL  PRIMARY KEY,
+    id             INT           NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
     title          VARCHAR(30)   NOT NULL,
     salary         DECIMAL       NOT NULL,
-    department_id  INT           NOT NULL,
+    department_id  INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
                                                                 /* ----------------- Table: Employee ------------------ */
 CREATE TABLE employee (
-    id           INT             NOT NULL  PRIMARY KEY,
+    id           INT             NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
     first_name   VARCHAR(30)     NOT NULL,
     last_name    VARCHAR(30)     NOT NULL,
-    role_id      INT             NOT NULL,
-    manager_id   INT             NOT NULL,
+    role_id      INT,
+    manager_id   INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
